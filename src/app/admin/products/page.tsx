d723cf0ca2fbfddece9b644158e3b48b521fdbb4
snow6692 <ahmedha258258@gmail.com>
2025-02-15 10:37:09 +0200

    Product created

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,9 +1,16 @@
 import { getCategoriesWithProducts } from "@/actions/categories";
 import ProductPageComponent from "./page-component";
+import { getProductsWithCategories } from "@/actions/products";
 async function Products() {
   const categories = await getCategoriesWithProducts();
+  const productsWithCategories = await getProductsWithCategories();
 
-  return <ProductPageComponent categories={categories} />;
+  return (
+    <ProductPageComponent
+      categories={categories}
+      productsWithCategories={productsWithCategories}
+    />
+  );
 }
 
 export default Products;
